338873bc36cd71ac1736420535daf2e1eff0b300
mohammadhoseinbab <sirhb77@gmail.com>
2025-12-13 18:27:18 +0100

    adding mui topbar and drawer and fix the overlap issue

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,5 +1,10 @@
 import { Outlet, Link } from "react-router-dom"
 
+interface MenuItem {
+  id: number;
+  title: string;
+}
+
 import {
   Box,
   Drawer,
@@ -12,15 +17,47 @@ import {
   Button
 } from "@mui/material";
 
+const listMenuLeft: MenuItem[] = [
+  { id: 0, title: "menu" },
+  { id: 1, title: "sale dashboard" },
+  { id: 2, title: "setting" },
+  { id: 3, title: "logout" }
+]
+
 export default function DashboardLayout() {
+  const drawerWidth = 240;
   return (
     <>
-      <AppBar position="static">
-        <Toolbar sx={{ justifyContent: "space-between" }}>
-          <Typography>Dashboard</Typography>
-          <Button color="inherit" component={Link} to="/login">Login</Button>
-        </Toolbar>
-      </AppBar>
+      <Box sx={{ display: 'flex' }}>
+        <Drawer
+          variant="permanent"
+          anchor="left"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Toolbar />
+          <List>
+            {listMenuLeft.map((item) => (
+              <ListItemButton key={item.id}>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
+            ))}
+          </List>
+        </Drawer>
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} >
+          <Toolbar sx={{ justifyContent: "space-between" }}>
+            <Typography>Dashboard</Typography>
+            <Button color="inherit" component={Link} to="/login">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
 
       <main>
         <Outlet />
